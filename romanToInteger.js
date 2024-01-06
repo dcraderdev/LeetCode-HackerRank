a33668077ed69ccb2d29e8733400a03c4bbe2b9b
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2024-01-06 08:50:54 -0800

    Update romanToInteger.js

--- romanToInteger.js
+++ romanToInteger.js
@@ -24,14 +24,14 @@
 const romanToInt = (s) => {
 
 
-  romanObj = {
-    I: 1,
-    V: 5,
-    X: 10,
-    L: 50,
-    C: 100,
-    D: 500,
-    M: 1000,
+  romanMap = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000,
   };
 
   let total = 0;
@@ -39,30 +39,51 @@ const romanToInt = (s) => {
 
   for(let i = 0; i < s.length; i++){
 
-    let curr = s[i]
+    let currChar = s[i]
+    let nextChar = s[i+1]
 
-    if(s[i+1] && romanObj[curr] < roman){
-      temp
+    
+
+
+    if(nextChar && romanMap[currChar] < romanMap[nextChar]){
+      total += romanMap[nextChar] - romanMap[currChar]
+    } else {
+      total += romanMap[currChar]
     }
+
 
   }
 
-
-
-
+return total
 
 };
 
+
+
+
+
+
+
+let s = "III"
+console.log(romanToInt(s));
 // Input: s = "III"
 // Output: 3
 // Explanation: III = 3.
 // Example 2:
 
+
+
+
+s = "LVIII"
+console.log(romanToInt(s));
 // Input: s = "LVIII"
 // Output: 58
 // Explanation: L = 50, V= 5, III = 3.
 // Example 3:
 
+
+s = "MCMXCIV"
+console.log(romanToInt(s));
 // Input: s = "MCMXCIV"
 // Output: 1994
 // Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
