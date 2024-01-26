0f009733f8d53743638cef52aa959836c7ad4c96
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2024-01-26 09:47:58 -0800

    Update removeDupesFromSortedArray.js

--- removeDupesFromSortedArray.js
+++ removeDupesFromSortedArray.js
@@ -28,8 +28,19 @@
  * @param {number[]} nums
  * @return {number}
  */
-var removeDuplicates = function(nums) {
-    
+  var removeDuplicates = function(nums) {
+    if (nums.length === 0) {
+        return 0;
+    }
+
+    let j = 0;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[j] !== nums[i]) {
+            nums[++j] = nums[i];
+        }
+    }
+
+    return j + 1;
 };
 
 // Example 1:
